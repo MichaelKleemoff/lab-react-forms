cb2640f746ba1f284416ff0f146357b84e611a1e
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-08-05 11:06:33 -0400

    First commit.

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,24 +1,34 @@
-import React from "react";
-import "./Form.css";
+import React from 'react';
+import { useState } from 'react';
+import './Form.css';
 
 function Form() {
-  return (
-    <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
-        </select>
-        <button type="submit">Calculate</button>
-      </form>
-      <section id="result">
-        <p></p>
-      </section>
-    </>
-  );
+	const [numbers, setNumbers] = useState([]);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		setNumbers([...numbers, e.target.value]);
+		console.log(e.target);
+	}
+
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<input id='values' name='values' type='text' />
+				<select id='operation' name='operation'>
+					<option value=''></option>
+					<option value='sum'>sum</option>
+					<option value='average'>average</option>
+					<option value='mode'>mode</option>
+				</select>
+
+				<button type='submit'>Calculate</button>
+			</form>
+			<section id='result'>
+				<p></p>
+			</section>
+		</>
+	);
 }
 
 export default Form;
